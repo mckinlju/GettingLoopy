a67bff25957b70dd074f722a89157bfd3e1cf99a
mckinlju <mckinlju@mail.uc.edu>
2023-10-16 16:09:47 -0400

    Part A + B

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,33 @@ public class Main
         {
             System.out.println(cnt);
         }
+
+        for(int row = 1; row <= 5; row++)
+        {
+            for(int col = 1; col <= row; col++ )
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int row = 5; row >= 1; row--)
+        {
+            for(int col = 1; col <= row; col++ )
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int row = 1; row <= 5; row++)
+        {
+            for(int col = 1; col <= 5; col++ )
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
 }
