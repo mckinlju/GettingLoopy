9818b4621c6e84d9824c828d481e56531e01bfff
mckinlju <mckinlju@mail.uc.edu>
2023-10-16 16:19:18 -0400

    Final Porject

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args)
@@ -48,6 +49,30 @@ public class Main
             }
             System.out.println();
         }
+
+        Scanner in = new Scanner(System.in);
+        double celsius = 0.0;
+        double fahrenheit = 0.0;
+        boolean done = false;
+
+        do
+        {
+            System.out.println("Please enter a temperature in Celsius: ");
+            if(in.hasNextDouble())
+            {
+                celsius = in.nextDouble();
+                fahrenheit = (celsius * 9/5) + 32;
+                done = true;
+            }
+            else
+            {
+
+                System.out.println("Please Enter a valid number");
+                in.next();
+            }
+        }while (!done);
+        System.out.println("The temperature in Fahrenheit is: " + fahrenheit);
+
     }
 
 }
